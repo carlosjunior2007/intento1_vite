c923658f46dd31a1573116fdef7229d2eff1a27d
Junior <jr.carlos.arcm@gmail.com>
2024-05-31 16:22:04 -0700

    prueba modificacion

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+          Si cambio algo se mira?
         </p>
       </div>
       <p className="read-the-docs">
